5451bf9d79f5d55504724b1f6de900a1ab4619da
ilci66 <77514750+ilci66@users.noreply.github.com>
2021-07-01 10:38:50 +0100

    navbar collapse fix

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -2,24 +2,26 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Navbar extends Component {
+
   render() {
-    return(
+    return (
       <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-      <Link to="/" className="navbar-brand">ExcerTracker</Link>
-      <div className="collapse navbar-collapse">
-        <ul>
+        <Link to="/" className="navbar-brand">ExcerTracker</Link>
+        {/* <div className="collapse navbar-collapse"> */}
+        <div className="navbar-collapse">
+        <ul className="navbar-nav mr-auto">
           <li className="navbar-item">
-            <Link to="/" className="nav-link">Exercises</Link>
+          <Link to="/" className="nav-link">Exercises</Link>
           </li>
           <li className="navbar-item">
-            <Link to="/create" className="nav-link">Create Exercise Log</Link>
+          <Link to="/create" className="nav-link">Create Exercise Log</Link>
           </li>
           <li className="navbar-item">
-            <Link to="/user" className="nav-link">Create User</Link>
+          <Link to="/user" className="nav-link">Create User</Link>
           </li>
         </ul>
-      </div>
-    </nav>
-    )
+        </div>
+      </nav>
+    );
   }
-} 
+}
